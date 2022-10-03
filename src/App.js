6e92a49f16f630139a197584ac1550fd116ebcb1
Christian Arroyo <ChristianArroyo308@yahoo.com>
2022-10-02 17:29:57 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useRef, useEffect } from 'react';
 
 function App() {
+
+  const [amount, setAmount] = useState(0)
+  // const [total, setTotal] = useState(0)
+  const hamPrice = 10.00
+
+  // const test = useRef(() => {
+  //   console.log("Hello")
+  // }, [amount])
+
+  useEffect(() => {
+    console.log("Hello");
+  }, [amount]);
+  
   return (
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div>Hamburger</div>
+        <div>Price: { hamPrice }</div>
+        <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} />
+
+        <p>Total = { amount * hamPrice}</p>
+
       </header>
     </div>
   );
