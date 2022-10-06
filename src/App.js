0b07333453a97116419b2c0f1cda32a3367133c8
Christian Arroyo <ChristianArroyo308@yahoo.com>
2022-10-05 19:00:28 -0700

    small update. adding Firebase configurations.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import './App.css';
 import { useState, useRef, useEffect } from 'react';
+import { db } from './firebase-config';
+import { collection, getDocs } from "firebase/firestore";
 
 function App() {
 
   const [amount, setAmount] = useState(0)
+  const [users, setUsers] = useState([])
+  const usersCollectionRef = collection(db, "users")
+
+
   // const [total, setTotal] = useState(0)
   const hamPrice = 10.00
 
-  // const test = useRef(() => {
-  //   console.log("Hello")
-  // }, [amount])
+
+  useEffect(() => {
+
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      console.log(data)
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    }
+
+    getUsers()
+  }, [])
 
   useEffect(() => {
     console.log("Hello");
@@ -19,6 +33,15 @@ function App() {
 
     <div className="App">
       <header className="App-header">
+
+        {users.map((user) => {
+          return (
+            <div>
+              <h1>Name: {user.name}</h1>
+              <h1>Age: {user.age}</h1>
+            </div>
+          )
+        })}
 
         <div>Hamburger</div>
         <div>Price: { hamPrice }</div>
